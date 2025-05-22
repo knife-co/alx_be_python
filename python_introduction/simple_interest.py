b822acc4b5b4a0d79b83c2998cd43fa93a6bb2e0
Nifemi <natomiwad@gmail.com>
2025-05-22 23:00:43 +0100

    Simple Interest Calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,15 @@
+# Create a file named simple_interest.py.
+# Define three variables in this file:
+# principal with a value of 1000 (representing $1000),
+# rate with a value of 0.05 (representing 5% annual interest rate),
+# time with a value of 3 (representing 3 years).
+# Calculate the simple interest using the formula provided and store the result in a variable named interest.
+# Print the calculated interest in a format: The simple interest is: [interest].
+
+p = 1000
+r = 0.05
+t = 3
+
+simple_interest = (p * r * t)/100
+
+print(f"The simple interest is {simple_interest}")
