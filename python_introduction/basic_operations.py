698f05f315aec70001fb14331ce4f4b2a2065a5f
Nifemi <natomiwad@gmail.com>
2025-05-22 22:53:55 +0100

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum = number2 + number1
+difference = number1 - number2
+product = number1 * number2 
+
+print(f"addition of {number2} and {number1} is {sum}")
+print(f"difference of {number1} and {number2} is {difference}")
+print(f"product of {number1} and {number2} is {product}")
