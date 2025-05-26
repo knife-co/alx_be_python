f35af5bb212f6519c45f3d098e26e5437a6e8232
Nifemi <natomiwad@gmail.com>
2025-05-26 21:48:17 +0100

    interest calculator correction

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -26,8 +26,8 @@
 # Your monthly savings are $1000.
 # Projected savings after one year, with interest, is: $12600.
 
-monthly_income = int(input("Enter your monthly income:"))
-monthly_expense = int(input("Enter your total monthly expenses:"))
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expense = int(input("Enter your total monthly expenses: "))
 
 monthly_savings = monthly_income - monthly_expense
 projected_savings = int((monthly_savings * 12) + (monthly_savings * 12 * 0.05))
