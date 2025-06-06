2eb4ddf088653868b54a935a5f8fbb168a0f016a
Nifemi <natomiwad@gmail.com>
2025-06-06 17:32:58 +0100

    arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            raise ValueError("Cannot divide by zero")
+    else:
+        raise ValueError(f"Invalid operation: {operation}. Use 'add', 'subtract', 'multiply', or 'divide'")
