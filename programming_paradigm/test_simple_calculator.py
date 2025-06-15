bc01f80e7127929725013f401588995a742edc71
Nifemi <natomiwad@gmail.com>
2025-06-15 23:11:34 +0100

    python tests

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,29 +4,29 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
     def test_add(self):
-        self.assertEqual(SimpleCalculator.add(10, 5), 15)
-        self.assertEqual(SimpleCalculator.add(-1, 1), 0)
-        self.assertEqual(SimpleCalculator.add(-1, -1), -2)
+        self.assertEqual(self.calc.add(10, 5), 15)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
     
     def test_subtract(self):
-        self.assertEqual(SimpleCalculator.subtract(10, 5), 5)
-        self.assertEqual(SimpleCalculator.subtract(-1, 1), -2)
-        self.assertEqual(SimpleCalculator.add(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.add(-1, -1), 0)
 
     def test_multiply(self):
-        self.assertEqual(SimpleCalculator.multiply(10, 5), 50)
-        self.assertEqual(SimpleCalculator.multiply(-1, 1), -1)
-        self.assertEqual(SimpleCalculator.multiply(-1, -1), 1)
+        self.assertEqual(self.calc.multiply(10, 5), 50)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
 
     def test_divide(self):
-        self.assertEqual(SimpleCalculator.divide(10, 5), 2)
-        self.assertEqual(SimpleCalculator.divide(-1, 1), -1)
-        self.assertEqual(SimpleCalculator.divide(-1, -1), 1)
+        self.assertEqual(self.calc.divide(10, 5), 2)
+        self.assertEqual(self.calc.divide(-1, 1), -1)
+        self.assertEqual(self.calc.divide(-1, -1), 1)
 
-        self.assertRaises(ValueError, SimpleCalculator.divide, 10, 0)
+        self.assertRaises(ValueError, self.calc.divide, 10, 0)
 
 
 if __name__ == '__main__':
